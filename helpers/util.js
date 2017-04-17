a3f0bfe74bbdb010b43325efaf47f83c26f37b71
edimdendidiwangga <edimdendy@gmail.com>
2017-04-17 19:42:19 +0700

    fix yeahhh

--- helpers/util.js
+++ helpers/util.js
@@ -1,10 +1,32 @@
 'use strict';
-
 var models = require('../models/index');
 var util = {};
-
-util.anagrams = function(source, callback){
-
+util.anagrams = function(input, callback) {
+  models.Word.findAll()
+    .then((allwords) => {
+      // get word only
+      let words = [];
+      allwords.forEach((word) => {
+        words.push(word.words)
+      })
+      // sort all words
+      let sortWords = [];
+      words.forEach(word => {
+        sortWords.push(word.split('')
+          .sort()
+          .join(''))
+      })
+      let target = input.split("")
+        .sort()
+        .join("")
+        .trim();
+      let indices = [];
+      let idx = sortWords.indexOf(target);
+      while (idx != -1) {
+        indices.push(words[idx]);
+        idx = sortWords.indexOf(target, idx + 1); //element , checkPosIndexElementnya
+      }
+      callback(words, indices)
+    })
 }
-
 module.exports = util;
--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,16 @@
 var express = require('express');
 var router = express.Router();
-var helper = require('../helper/util')
+var helper = require('../helpers/util')
 var models = require('../models')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  models.Word.findAll()
-  .then((allwords)=>{
-    let source = [];
-    allwords.forEach((word)=>{
-      source.push(word.words)
-    })
-    let data = req.query.word || 'apa aja';
-
-    let result = helper.anagrams(source, data);
-    //res.send({ output: result || '??', input : data})
-    //console.log(source)
-     res.render('index', { alldata : allwords, output: result || '??', input : data || 'word'})
-  })
-
+    let input = req.query.word || 'words';
+    //let testArray = 'anagrams' in helper.util;
+    //res.send(testArray)
+    helper.anagrams(input, (source, result)=>{
+      res.render('index', { alldata : source, output: result || '??', input : input || 'word'})
+    });
 });
 
 module.exports = router;
