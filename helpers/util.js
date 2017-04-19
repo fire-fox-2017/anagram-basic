777078c847eec9cfd9f5aaea3beba756804a7671
parelhutahaean <parel.hutahaean@gmail.com>
2017-04-20 04:43:40 +0700

    ParelHutahaean Finished

--- helpers/util.js
+++ helpers/util.js
@@ -1,9 +1,32 @@
 'use strict';
 
-var models = require('../models/index');
+var models = require('../models/word');
+var db = require('../models');
 var util = {};
 
+function simplify(word) {
+  let simple = '';
+  for(var i = 0; i < word.length; i++) {
+    if (/[^\']/.test(word[i])) {
+      simple += word[i].toLowerCase();
+    }
+  }
+  return simple.split('').sort().join('');
+}
+
 util.anagrams = function(source, callback){
+  let word1 = simplify(source);
+  let data = [], dictionary = [];
+  db.Word.findAll()
+  .then(words => {
+    words.forEach(word => {
+      if (word1 === simplify(word.word)) {
+        data.push(word.word);
+      }
+      dictionary.push(word.word);
+    });
+    callback(dictionary, data);
+  });
 
 }
 
--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,14 @@ var router = express.Router();
 var helper = require('../helpers/util');
 
 /* GET home page. */
-
 router.get('/', function(req, res, next) {
-  var word = req.query.word;
-
-  // release 2
-  res.render('index', {title: 'Anagrams', word: word, anagrams: {} });
-
-  // release 3
-  // helper.anagrams(word, function(source, data){
-  //   res.render('index', { title: 'Anagrams', word: source, anagrams: data });
-  // });
-
+  let word = req.query.word || '';
+  helper.anagrams(word, function(source, data) {
+    console.log(source)
+    res.render('index', {title:'Anagrams', word:word, source:source, anagrams: data});
+  });
 });
+
+router
 
 module.exports = router;
