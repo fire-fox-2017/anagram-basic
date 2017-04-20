c7be2a3461a9ab0011217c17b04258d819d146f2
haryanawisnu <arya.wisnu05@gmail.com>
2017-04-21 01:05:08 +0700

    all

--- helpers/util.js
+++ helpers/util.js
@@ -3,8 +3,24 @@
 var models = require('../models/index');
 var util = {};
 
-util.anagrams = function(source, callback){
-
+util.anagrams = function(source, callback) {
+  let dictionary = []
+  let anagrams = []
+  models.Words.findAll()
+    .then(words => {
+      for (var i = 0; i < words.length; i++) {
+        console.log(source + ' ' + words[i].words);
+        if (isAnagram(source, words[i].words)) {
+          anagrams.push({
+            words: words[i].words
+          })
+        }
+      }
+      callback(source, anagrams)
+    })
+    .catch(err => {
+      callback(err, '')
+    })
 }
 
 module.exports = util;
